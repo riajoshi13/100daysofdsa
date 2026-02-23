31139bf5d573aebeed977779f6b0fba870c6ebc8
riajoshi13 <Ria.26070@stu.upes.ac.in>
2026-02-23 15:57:07 +0530

    Day 20 - Count Subarrays with Sum Zero

---
+++ day20.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Read size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Read array
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int count = 0;
+
+    // Check all subarrays
+    for(int i = 0; i < n; i++) {
+        int sum = 0;
+
+        for(int j = i; j < n; j++) {
+            sum += arr[j];
+
+            if(sum == 0) {
+                count++;
+            }
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
