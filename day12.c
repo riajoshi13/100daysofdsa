054f7f028e2d07276de9da30407d92bf7ec80060
riajoshi13 <Ria.26070@stu.upes.ac.in>
2026-02-23 15:38:15 +0530

    Day 12 - Check Symmetric Matrix

---
+++ day12.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    // Read rows and columns
+    scanf("%d %d", &m, &n);
+
+    // If not square matrix → directly not symmetric
+    if (m != n) {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+
+    int matrix[m][n];
+
+    // Read matrix
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Check symmetry
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] != matrix[j][i]) {
+                printf("Not a Symmetric Matrix");
+                return 0;
+            }
+        }
+    }
+
+    printf("Symmetric Matrix");
+
+    return 0;
+}
