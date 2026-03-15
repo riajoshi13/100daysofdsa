d32ca28eca63b0b5102577728d64f93829f20fe4
riajoshi13 <Ria.26070@stu.upes.ac.in>
2026-03-15 11:26:48 +0530

    Day 35 - Queue implementation using array

---
+++ day35.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int queue[MAX];
+int front = 0, rear = -1;
+
+void enqueue(int value) {
+    if (rear == MAX - 1)
+        return;
+
+    rear++;
+    queue[rear] = value;
+}
+
+void display() {
+    for (int i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+}
+
+int main() {
+
+    int n, x;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    display();
+
+    return 0;
+}
