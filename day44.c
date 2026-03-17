2bdbd9803b47bf5726b8c4de9488017868ec2522
riajoshi13 <Ria.26070@stu.upes.ac.in>
2026-03-17 14:25:55 +0530

    Day 44 - Binary tree traversals (inorder, preorder, postorder)

---
+++ day44.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Build tree from level order
+struct Node* buildTree(int arr[], int n) {
+
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+
+        if (nodes[i] != NULL) {
+
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if (left < n)
+                nodes[i]->left = nodes[left];
+
+            if (right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    return nodes[0];
+}
+
+// Traversals
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void preorder(struct Node* root) {
+    if (root == NULL) return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(struct Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    inorder(root);
+    printf("\n");
+
+    preorder(root);
+    printf("\n");
+
+    postorder(root);
+
+    return 0;
+}
