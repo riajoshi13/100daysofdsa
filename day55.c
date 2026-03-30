46d2a83cf02c76022634ed3e660f309b5597d4f2
riajoshi13 <Ria.26070@stu.upes.ac.in>
2026-03-31 00:06:49 +0530

    Day 55 - Right view of binary tree

---
+++ day55.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Build tree from level order
+struct Node* buildTree(int arr[], int n) {
+
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if (left < n)
+                nodes[i]->left = nodes[left];
+
+            if (right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    return nodes[0];
+}
+
+// Right View Function
+void rightView(struct Node* root) {
+
+    if (root == NULL)
+        return;
+
+    struct Node* queue[100];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+
+        int size = rear - front;
+
+        for (int i = 0; i < size; i++) {
+
+            struct Node* curr = queue[front++];
+
+            // print last node of level
+            if (i == size - 1)
+                printf("%d ", curr->data);
+
+            if (curr->left)
+                queue[rear++] = curr->left;
+
+            if (curr->right)
+                queue[rear++] = curr->right;
+        }
+    }
+}
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    rightView(root);
+
+    return 0;
+}
