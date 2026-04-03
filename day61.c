5320d32493368c5191c97730e58edb3bb4ec829c
riajoshi13 <Ria.26070@stu.upes.ac.in>
2026-04-03 13:43:16 +0530

    Day 61 - Graph using adjacency matrix

---
+++ day61.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, m;
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    int adj[n][n];
+
+    // initialize matrix with 0
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    // input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        adj[u][v] = 1;
+        adj[v][u] = 1; // remove this line for directed graph
+    }
+
+    // print matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", adj[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
