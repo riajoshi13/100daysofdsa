31fb91e113e6593d8c31fa09c83d2e68b9c97b81
riajoshi13 <Ria.26070@stu.upes.ac.in>
2026-04-13 17:11:32 +0530

    Day 72 - First repeated character using hashing

---
+++ day72.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char str[1000];
+    scanf("%s", str);
+
+    int visited[26] = {0};
+
+    for (int i = 0; str[i] != '\0'; i++) {
+
+        int index = str[i] - 'a';
+
+        if (visited[index] == 1) {
+            printf("%c", str[i]);
+            return 0;
+        }
+
+        visited[index] = 1;
+    }
+
+    printf("-1");
+    return 0;
+}
