2b1c9221df7aa1d7c44b003c26e9379b14d41595
riajoshi13 <Ria.26070@stu.upes.ac.in>
2026-04-17 22:31:55 +0530

    Day 76 - Count connected components using DFS

---
+++ day76.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+// Node structure
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* adj[MAX];
+int visited[MAX];
+
+// Create node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Add edge (undirected)
+void addEdge(int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = adj[v];
+    adj[v] = newNode;
+}
+
+// DFS
+void DFS(int v) {
+    visited[v] = 1;
+
+    struct Node* temp = adj[v];
+
+    while (temp != NULL) {
+        if (!visited[temp->vertex]) {
+            DFS(temp->vertex);
+        }
+        temp = temp->next;
+    }
+}
+
+int main() {
+
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    // initialize
+    for (int i = 1; i <= n; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+    }
+
+    // input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);
+    }
+
+    int components = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            DFS(i);
+            components++;
+        }
+    }
+
+    printf("%d", components);
+
+    return 0;
+}
