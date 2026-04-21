9d1f756796df6c74c49e0479a7c6f50c812e9f11
riajoshi13 <Ria.26070@stu.upes.ac.in>
2026-04-21 10:48:19 +0530

    Day 80 - Floyd Warshall algorithm

---
+++ day80.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+#define MAX 100
+#define INF 99999
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int dist[MAX][MAX];
+
+    // input matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+
+            if (dist[i][j] == -1 && i != j) {
+                dist[i][j] = INF;
+            }
+        }
+    }
+
+    // Floyd-Warshall
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (dist[i][k] != INF && dist[k][j] != INF &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // output
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+
+            if (dist[i][j] == INF)
+                printf("-1 ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
